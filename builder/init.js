fe2022eae0f2d802928c0369a582b66ae00eb43d
Faisal Alfainun <57319059+ryouzaa@users.noreply.github.com>
2023-02-22 16:17:55 +0700

    Update init.js

--- builder/init.js
+++ builder/init.js
@@ -45,7 +45,7 @@ const setRemoteData = async () => {
     if (msgPath) {
       const article = msgPath.split("/").pop();
       res = await axios.get(
-        `https://api.telegra.ph/getPage/${article}?return_content=true`
+        'https://api.telegra.ph/getPage/${article}?return_content=true'
       );
       const { content } = res.data.result;
       markup = content.reduce(
